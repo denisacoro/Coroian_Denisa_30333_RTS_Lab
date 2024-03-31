b2cedfbd1f0c0aba498b3a0bc506eb342311a1da
denisacoro <denisacoroian808@gmail.com>
2024-03-31 20:32:29 +0300

    Labsession2 updated

--- Project/src/LabSession2/Example3/JoinTestThread.java
+++ Project/src/LabSession2/Example3/JoinTestThread.java
@@ -1,54 +1,33 @@
 package LabSession2.Example3;
 
-class JoinTestThread extends Thread {
-    private static int sumOfDivisors = 0;
-    private Thread t;
+public class JoinTestThread extends Thread{
+    public static int sum = 0;
+    public Thread thread;
 
-    JoinTestThread(String n, Thread t) {
-        this.setName(n);
-        this.t = t;
+    int number = 60000;
+    int localSum = 0;
+    public JoinTestThread(String name, Thread thread) {
+        this.thread = thread;
+        this.setName(name);
     }
-
     public void run() {
-        System.out.println("Thread " + getName() + " has entered the run() method");
+        System.out.println("Thread" + this.getName() + " has entered the run() method");
         try {
-            if (t != null) t.join();
-            System.out.println("Thread " + getName() + " executing operation.");
-
-            // Determine divisors and calculate sum
-            int num;
-            if (getName().equals("Thread 1")) {
-                num = 50001; // Start checking from 50001
-            } else if (getName().equals("Thread 2")) {
-                num = 20001; // Start checking from 20001
-            } else {
-                System.out.println("Invalid thread name.");
-                return;
-            }
-
-            int divisorSum = 0;
-            for (int i = 1; i <= num; i++) {
-                if (num % i == 0) {
-                    divisorSum += i;
+            if (thread != null) thread.join();
+            System.out.println("Thread " + this.getName() + " executing operation.");
+            for (int i = 1; i <= this.number; i++) {
+                if (this.number % i == 0) {
+                    this.localSum += i;
                 }
             }
-
-            // Assign sum to static variable in main class
-            if (getName().equals("Thread 1")) {
-                sumOfDivisors = divisorSum;
-            } else if (getName().equals("Thread 2")) {
-                sumOfDivisors += divisorSum;
+            synchronized(this) {
+                JoinTestThread.sum += this.localSum;
             }
+            System.out.println("Thread " + this.getName() + " has terminated operation, with value: " + Integer.toString(JoinTestThread.sum));
 
-            Thread.sleep(3000);
-            System.out.println("Thread " + getName() + " has terminated operation.");
+
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
-    public static int getSumOfDivisors() {
-        return sumOfDivisors;
-    }
 }
-
--- Project/src/LabSession2/Example3/Main.java
+++ Project/src/LabSession2/Example3/Main.java
@@ -1,19 +1,10 @@
 package LabSession2.Example3;
 
 public class Main {
-    public static void main(String[] args) {
-        JoinTestThread w1 = new JoinTestThread("Thread 1", null);
-        JoinTestThread w2 = new JoinTestThread("Thread 2", w1);
+    public static void main(String [] args) {
+        JoinTestThread w1 = new JoinTestThread("Thread1", null);
+        JoinTestThread w2 = new JoinTestThread("Thread2", w1);
         w1.start();
         w2.start();
-
-        try {
-            w1.join();
-            w2.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println("Sum of divisors: " + JoinTestThread.getSumOfDivisors());
     }
 }
